d351a60fa70d2aa8daa76651dd328288b54051e4
MoonTahoe <alex@moonhighway.com>
2018-07-07 19:02:07 -0700

    attacking this bug

--- server.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-var { mockH5Server } = require('./')
-
-mockH5Server.listen(process.env.port || 4000)
-    .then(({ url }) => `Mock High Fives GraphQL Service running at ${url}`)
-    .then(console.log)
-    .catch(console.error)
